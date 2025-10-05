60a4bd39a0f93b64166a12bdc796a7c2313d0640
BenerdAndrian <ben527466@gmail.com>
2025-10-05 05:06:27 +0000

    Added reused general layout components

---
+++ SWE Project/front_end/components/Layouts/Sidebar.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { NavModule} from './NavModule'
+import { useState } from 'react'
+
+export function Sidebar({list=[]}:any){
+  const [activeIndex,setActiveIndex] = useState(null)
+
+  const handleToggle=(index)=>{
+    if(activeIndex===index)
+    setActiveIndex(null);
+    else
+    setActiveIndex(index)
+  }
+
+  const showSubModule = (subModules, activeIndex, index) => {
+  return (
+    <div
+      className={`overflow-hidden transition-all duration-500 ease-in-out ${
+        activeIndex===index ? 'max-h-96 opacity-100 mt-3' : 'max-h-0 opacity-0'
+      }`}
+    >
+      <ul className="flex flex-col gap-3">
+        {subModules.map((subModule, index) => (
+          
+          <li key={`${index}-${subModule.text}`}>
+            <NavModule
+              isSubModule={true}
+              item={subModule}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+  return(
+    <>
+      <div style={{backgroundColor:'white'}} className="pt-5 h-full w-full">
+
+          <ul className="flex flex-col flex-start gap-[1.5rem]">
+            {list.map((item,index)=>(
+              
+                <li key={`${index}-${item.text}`}>
+                   <NavModule isActiveModule={activeIndex===index} item={item} setIndex={()=>{handleToggle(index)}}/>
+                   {item.subModules && showSubModule(item.subModules,activeIndex,index)}
+                </li>
+            ))}
+          </ul>
+      </div>
+    </>
+  )
+}
---
+++ SWE Project/front_end/models/admin/adminSidebarModel.ts
@@ -0,0 +1,52 @@
+import { GrOverview,GrSchedule } from "react-icons/gr";
+import { BsFillPersonVcardFill } from "react-icons/bs";
+import { PiStudentBold } from "react-icons/pi";
+import { RiParentFill } from "react-icons/ri";
+import { ImRoad } from "react-icons/im";
+import { TbScanPosition } from "react-icons/tb";
+import { FaBusAlt } from "react-icons/fa";
+
+export const adminSidebarModel = [
+    {
+        text:'Overview',
+        url: '/Overview',
+        icon: GrOverview
+    },
+    {
+        text: 'Drivers',
+        url: '/Drivers',
+        icon: BsFillPersonVcardFill
+    },
+    {
+        text: 'Students',
+        url: '/Students',
+        icon: PiStudentBold
+    },
+    {
+        text: 'Parents',
+        url: '/Parents',
+        icon: RiParentFill
+    },
+    {
+        text: 'Tracks',
+        url: '/Tracks',
+        icon: TbScanPosition,
+        subModules:[
+            {
+                text:'Schedules',
+                url:'/Schedules',
+                icon: GrSchedule
+            },
+            {
+                text:'Routes',
+                url:'/Routes',
+                icon: ImRoad
+            },
+            {
+                text:'Buses',
+                url:'/Buses',
+                icon: FaBusAlt
+            },
+        ]
+    }
+]
